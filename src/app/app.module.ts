9c4b6a1afe92150fe8724943c9851a0e56fe9a01
Pravin Bhalghare <pravinbhalghare1992@gmail.com>
2021-05-05 11:07:04 +0530

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
+import { CreateCommunityComponent } from './community/create-community/create-community.component';
+import { ViewCommunityComponent } from './community/view-community/view-community.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponent,
+    ViewPostComponent,
+    CreateCommunityComponent,
+    ViewCommunityComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
