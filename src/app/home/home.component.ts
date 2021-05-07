d8bb3067a6e69c5e8ba837253add71017ba1d5e5
ZafarAqil <zafaraqil14@gmail.com>
2021-05-07 22:08:21 +0530

    fixed UI and added proper community routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { CommunityService } from '../shared/community.service';
+import { TokenStorageService } from 'src/app/shared/token-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  communities:any[]=[];
-  error:any='';
-  constructor(private communityService:CommunityService) { }
+  communities: any[] = [];
+  error: any = '';
+  isLoggedIn: boolean = false;
+  constructor(
+    private communityService: CommunityService,
+    private tokenService: TokenStorageService
+  ) {}
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLoggedIn = true;
+    }
     this.communityService.getCommunities().subscribe(
-      data => {
+      // data => {
+      //   this.communities = data;
+      // },
+      (data) => {
+        console.log(data);
         this.communities = data;
+
+        if (data.length > 4) {
+          this.communities = data.reverse().splice(0, 4);
+        } else {
+          this.communities = data;
+        }
       },
-      err => {
+      (err) => {
         this.error = JSON.parse(err.error).message;
       }
     );
   }
+}​​​​​​​
+ 
 
-}
+
+
